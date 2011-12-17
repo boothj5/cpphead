d2b7bcf325108d73475543eb7c093aa0551d664d
James Booth <boothj5@gmail.com>
2011-12-17 19:33:20 +0000

    Added console

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 rm -f ./bin/*
 
 g++ -c player.cpp
+g++ -c console.cpp
 g++ -c game.cpp
-g++ player.o game.o cpphead.cpp -o ./bin/cpphead
+g++ console.o player.o game.o cpphead.cpp -o ./bin/cpphead
 
