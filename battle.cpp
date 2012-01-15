da02416fc6513fe1789c88dbaf574a3691f2c19a
James Booth <boothj5@gmail.com>
2012-01-15 14:31:26 +0000

    Battle stats

--- battle.cpp
+++ battle.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+#include <iomanip>
 #include <sstream>  
 #include <vector>
 #include <string>
+#include <ctime>
+#include <algorithm>
 #include <map>
 #include "player_factory.hpp"
 #include "player.hpp"
@@ -13,9 +16,19 @@
 
 using namespace std;
 
+bool scoreOrder(pair<string, int> player1, pair<string, int> player2)
+{
+    return player1.second < player2.second;
+}
+
 int main(int argc, char *argv[])
 {
+    clock_t start, end;
+    double totalTime;
+    float avgGameTime;
+    
     typedef map<string, int> shMap_t;
+    typedef vector<pair<string, int> > shVec_t;
     shMap_t shMap;
     
     int numGames;
@@ -30,6 +43,8 @@ int main(int argc, char *argv[])
     shMap["HighCardPlayer"] = 0;
     shMap["Pyromaniac"] = 0;
             
+    start = clock();
+
     try {
         for (game = 0; game < numGames; game++) {
             vector<string> names;
@@ -80,13 +95,51 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    shMap_t::const_iterator iter;
-    for (iter = shMap.begin(); iter!=shMap.end(); iter++) {
-        string name = iter->first;
-        cout << name << ": " << shMap[name] << endl;
+    end = clock();
+    totalTime = (end - start) / (CLOCKS_PER_SEC / 1000);
+    avgGameTime = totalTime / numGames;
+
+    cout << endl << "SUMMARY" << endl;
+    cout << "Total games : " << numGames << endl;
+    
+    if (totalTime >= 60000) {
+        int minutes = totalTime / 60000;
+        int seconds = (totalTime - (minutes * 60000)) / 1000;
+        cout << "Total time : " << minutes << " minutes, " << seconds << " seconds" << endl;
+    } else if (totalTime >= 1000) {
+        int seconds = totalTime / 1000;
+        cout << "Total time : " << seconds << " seconds" << endl;
+    } else {
+        cout << "Total time : " << totalTime << " milliseconds" << endl;
     }
 
-    cout << endl << "Stalemates: " << stalemates << endl;
+    cout << "Average game: " << avgGameTime << " milliseconds" << endl;
+
+    float stalematePercent = ((double)stalemates / (double)numGames) * 100.0;
+    cout << "Stalemates: " << stalemates << ", " << stalematePercent << "%" << endl;
+    cout << endl;
+
+    cout << "SCORES" << endl;
+
+    shVec_t shVec;
+    copy(shMap.begin(), shMap.end(), back_inserter(shVec));
+    sort(shVec.begin(), shVec.end(), scoreOrder);
+
+    cout << setw(20) << left << "Name:";
+    cout << setw(10) << "Shithead";
+    cout << setw(10) << "Lose rate" << endl;
+    cout << setfill('-') << setw(40) << "-" << endl;
+    cout << setfill(' ');
+
+    shVec_t::const_iterator iter;
+    for (iter = shVec.begin(); iter!=shVec.end(); iter++) {
+        float percentSh = ((double)iter->second / (double)numGames) * 100.0;
+        cout << setw(20) << left << iter->first;
+        cout << setw(10) << iter->second;
+        cout << setprecision(2) << percentSh << "%" << endl;
+    }
+    
+    cout << endl;
 
     return 0;
 }
