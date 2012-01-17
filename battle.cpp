7ad002f21ebe5bc76e7d76fbb11ced171725e8b2
James Booth <boothj5@gmail.com>
2012-01-17 22:33:44 +0000

    Tidied battle

--- battle.cpp
+++ battle.cpp
@@ -25,57 +25,62 @@ bool scoreOrder(pair<string, int> player1, pair<string, int> player2)
 
 int main(int argc, char *argv[])
 {
+    typedef pair<char, string> typeName_t;
+    typedef map<string, int> shMap_t;
+    typedef vector<pair<string, int> > shVec_t;
+
     clock_t start, end;
     double totalTime;
     float avgGameTime;
-    
-    typedef map<string, int> shMap_t;
-    typedef vector<pair<string, int> > shVec_t;
+    int numGames, game;
+    int stalemates = 0;
     shMap_t shMap;
-    
-    int numGames;
     
     stringstream(argv[1]) >> numGames;
 
-    int game;
-    int stalemates = 0;
+    // initialise player list
+    vector<typeName_t> pVec;
+    pVec.push_back(make_pair('s', "SimplePlayer"));
+    pVec.push_back(make_pair('l', "LowCardPlayer"));
+    pVec.push_back(make_pair('p', "Pyromaniac"));
 
-    shMap["SimplePlayer"] = 0;
-    shMap["LowCardPlayer"] = 0;
-    shMap["Pyromaniac"] = 0;
+    // initialise shithead map
+    vector<typeName_t>::const_iterator tnIter;
+    for (tnIter = pVec.begin(); tnIter!=pVec.end(); tnIter++) {
+        shMap[tnIter->second] = 0;
+    }
             
     start = clock();
 
     try {
         for (game = 0; game < numGames; game++) {
-            vector<pair<char, string> > pVec;
-            pVec.push_back(make_pair('s', "SimplePlayer"));
-            pVec.push_back(make_pair('l', "LowCardPlayer"));
-            pVec.push_back(make_pair('p', "Pyromaniac"));
+            int i;           
+ 
+            // make sure players in different order for each game
+            util::shuffle<typeName_t>(pVec);
 
-            util::shuffle<pair<char, string> >(pVec);
-
+            // set up player names and types for game creation
             vector<string> names;
             vector<char> types;
-
-            vector<pair<char, string> >::const_iterator iter;
+            vector<typeName_t>::const_iterator iter;
             for (iter = pVec.begin(); iter!=pVec.end(); iter++) {
                 types.push_back(iter->first);
                 names.push_back(iter->second);
             }
 
             Game *game = new Game(names, types, NUM_CARDS);
+            
             game->deal();
 
+            // ask players to swap
             vector<Player *> players = game->players();
-            int i;    
             for (i = 0 ; i < players.size() ; i++)
                 player_swap(players[i], *game);
 
             game->firstMove();
 
+            // main game play, count moves for stalemates
             int moves = 0;        
-    
             while (game->canContinue() && moves < THRESHOLD) {
                 const Player *currentPlayer = game->currentPlayer();
                 if (game->playingFromFaceDown())
@@ -86,10 +91,11 @@ int main(int argc, char *argv[])
                 moves++;
             }
 
+            // log stalemates if game didn't complete
             if (moves == THRESHOLD) {
                 stalemates++;
             }
-            else {
+            else { // add to shithead count for loser
                 string loser = game->getCppHead();
                 ++shMap[loser];       
             }
@@ -101,6 +107,7 @@ int main(int argc, char *argv[])
     }
 
     end = clock();
+
     totalTime = (end - start) / (CLOCKS_PER_SEC / 1000);
     avgGameTime = totalTime / numGames;
 
@@ -110,38 +117,49 @@ int main(int argc, char *argv[])
     if (totalTime >= 60000) {
         int minutes = totalTime / 60000;
         int seconds = (totalTime - (minutes * 60000)) / 1000;
-        cout << "Total time : " << minutes << " minutes, " << seconds << " seconds" << endl;
+        cout << "Total time : ";
+        cout << minutes << " minutes, "; 
+        cout << seconds << " seconds" << endl;
     } else if (totalTime >= 1000) {
         int seconds = totalTime / 1000;
-        cout << "Total time : " << seconds << " seconds" << endl;
+        cout << "Total time : "; 
+        cout << seconds << " seconds" << endl;
     } else {
-        cout << "Total time : " << totalTime << " milliseconds" << endl;
+        cout << "Total time : ";
+        cout << totalTime << " milliseconds" << endl;
     }
 
-    cout << "Average game: " << avgGameTime << " milliseconds" << endl;
+    cout << "Average game: "; 
+    cout << avgGameTime << " milliseconds" << endl;
 
     float stalematePercent = ((double)stalemates / (double)numGames) * 100.0;
-    cout << "Stalemates: " << stalemates << ", " << stalematePercent << "%" << endl;
+
+    cout << "Stalemates: ";
+    cout << stalemates << ", " << stalematePercent << "%" << endl;
     cout << endl;
 
     cout << "SCORES" << endl;
 
+    // copy the shMap to a vector and sort it by score
     shVec_t shVec;
     copy(shMap.begin(), shMap.end(), back_inserter(shVec));
     sort(shVec.begin(), shVec.end(), scoreOrder);
 
-    cout << setw(20) << left << "Name:";
+    cout << left;
+    cout << setw(20) << "Name:";
     cout << setw(10) << "Shithead";
-    cout << setw(10) << "Lose rate" << endl;
-    cout << setfill('-') << setw(40) << "-" << endl;
+    cout << setw(10) << "Lose rate";
+    cout << endl;
+    cout << setw(40) << setfill('-') << "-" << endl;
     cout << setfill(' ');
 
     shVec_t::const_iterator iter;
     for (iter = shVec.begin(); iter!=shVec.end(); iter++) {
         float percentSh = ((double)iter->second / (double)numGames) * 100.0;
-        cout << setw(20) << left << iter->first;
+        cout << setw(20) << iter->first;
         cout << setw(10) << iter->second;
-        cout << setprecision(2) << percentSh << "%" << endl;
+        cout << setprecision(2);
+        cout << percentSh << "%" << endl;
     }
     
     cout << endl;
